6a025274c66b989a50a2e427a2e87f3c6ca50c0e
fafathr <fatimaataheriiot77@gmail.com>
2024-06-08 17:40:02 +0300

    first commit

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -3,7 +3,7 @@ import BuyResidentialsPage from "@/template//BuyResidentialsPage";
 
 async function BuyResidentials({ searchParams }) {
   // بهتر است در کامپوننت های سرورساید از ای پی آی روت استفاده نکنیم(این مورد حالت تمرینی دارد)
-  const res = await fetch("http://localhost:3000/api/profile", {
+  const res = await fetch("https://projectrealstates.vercel.app//api/profile", {
     cache: "no-store",
   });
   const data = await res.json();
